86d6458ec675c092968188ec5bc5e6c748bd5134
DeryckeTayron <tayron.derycke@student.howest.be>
2024-08-20 12:00:53 +0200

    added editable name

--- vend_app/lib/pages/settings_page.dart
+++ vend_app/lib/pages/settings_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:vend_app/auth.dart';
@@ -14,6 +15,57 @@ class MySettingsPage extends StatefulWidget {
 
 class _MySettingsPageState extends State<MySettingsPage> {
   final User? currentUser = Auth().currentUser;
+
+  String? _name;
+  String? _firstName;
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _firstNameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
+  Future<void> _fetchUserData() async {
+    final user = Auth().currentUser;
+    if (user != null) {
+      final doc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+      setState(() {
+        _name = doc['name'];
+        _firstName = doc['firstName'];
+        _nameController.text = _name ?? '';
+        _firstNameController.text = _firstName ?? '';
+      });
+    }
+  }
+
+  Future<void> _saveChanges() async {
+    final user = Auth().currentUser;
+    if (user != null) {
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .update({
+        'name': _nameController.text,
+        'firstName': _firstNameController.text,
+      });
+      setState(() {
+        _name = _nameController.text;
+        _firstName = _firstNameController.text;
+      });
+
+      // Show a success message using a SnackBar or a dialog
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Changes saved successfully'),
+        ),
+      );
+    }
+  }
 
   Future<void> signOut() async {
     await Auth().signOut();
@@ -37,6 +89,51 @@ class _MySettingsPageState extends State<MySettingsPage> {
     return ElevatedButton(onPressed: signOut, child: const Text('Sign Out'));
   }
 
+  Widget _editNameAndFirstNameButton() {
+    return ElevatedButton(
+      onPressed: () {
+        _showEditModal(context);
+      },
+      child: const Text('Edit Name'),
+    );
+  }
+
+  void _showEditModal(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Edit Name'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _nameController,
+                decoration: const InputDecoration(labelText: 'Name'),
+              ),
+              TextField(
+                controller: _firstNameController,
+                decoration: const InputDecoration(labelText: 'First Name'),
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: _saveChanges,
+              child: const Text('Save Changes'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,6 +144,10 @@ class _MySettingsPageState extends State<MySettingsPage> {
       body: ListView(
         children: [
           const ListTile(
+            title: Text('General',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          ),
+          const ListTile(
             title: Text('Dark Mode'),
             trailing: DarkModeToggleButton(),
           ),
@@ -56,6 +157,15 @@ class _MySettingsPageState extends State<MySettingsPage> {
               Navigator.push(context,
                   MaterialPageRoute(builder: (context) => const AboutUsPage()));
             },
+          ),
+          const Opacity(opacity: 0.5, child: Divider()),
+          const ListTile(
+            title: Text('Account',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          ),
+          ListTile(
+            title: const Text('User Information'),
+            trailing: _editNameAndFirstNameButton(),
           ),
           ListTile(
             title: _userUid(),
--- vend_app/lib/widgets/marker_popup.dart
+++ vend_app/lib/widgets/marker_popup.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:vend_app/models/vending_machine.dart';
 
 class VendingMachineMarkerPopup extends StatelessWidget {
